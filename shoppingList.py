3d78d65f3fc26f4c43fe1eca0f4e98128c59a401
Austin Parsons <parsons_aj@comcast.net>
2016-03-06 17:47:59 -0500

    added comments to program.

--- shoppingList.py
+++ shoppingList.py
@@ -8,25 +8,24 @@ if os.path.isfile(fileName):
 	shoppingList = f.read().splitlines()
 else:
 	print("That file does not exist. A file called shoppingList.txt was created. ")
-	f = open("shoppingList.txt", "w")
-	shoppingList = []
-
-grocery = None 
+	f = open("shoppingList.txt", "w") #creates file if one doesn't exist
+	shoppingList = []	
+grocery = None #gets into the loop
 while grocery != "":
-	if len(shoppingList) == 0:
+	if len(shoppingList) == 0: 
 		print("Your List is empty. Press enter to quit. Or add an item.")
 	else:
 		print("your shopping List contains: ")
 		numberItem = 0
-		for grocery in shoppingList:
-			numberItem += 1
+		for grocery in shoppingList: 
+			numberItem += 1 #creates the nuber before each item
 			print("{}. {}".format(numberItem, grocery)) 
 	grocery = input("Add an item: ")
-	if grocery != "":
+	if grocery != "": #you dont want to append the space when you quit
 		shoppingList.append(grocery)
 f.seek(0)
 f.truncate()	
 for grocery in shoppingList:
 	f.write(grocery + '\n')
-f.close()
-exit()
+f.close() # if grocery does equal "" now the program will quit
+
