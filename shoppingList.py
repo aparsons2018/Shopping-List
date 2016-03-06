dc393786a605e089af54417a91dd7d862e640bf6
Austin Parsons <parsons_aj@comcast.net>
2016-03-06 17:40:51 -0500

    Fully functional shoppinglist program.

--- shoppingList.py
+++ shoppingList.py
@@ -4,27 +4,29 @@ import os
 fileName = input("enter the file name: ")
 if os.path.isfile(fileName):
 	f = open(fileName, 'r+')
-	f.truncate()
-	f.seek(0)
-	shoppingList = f.read()
-	shoppingList = f.splitlines()
+		#truncate when you are done with the list.
+	shoppingList = f.read().splitlines()
 else:
 	print("That file does not exist. A file called shoppingList.txt was created. ")
 	f = open("shoppingList.txt", "w")
 	shoppingList = []
-grocery = "start shopping"
+
+grocery = None 
 while grocery != "":
 	if len(shoppingList) == 0:
 		print("Your List is empty. Press enter to quit. Or add an item.")
-		grocery = input("Add an item: ")
-		shoppingList.append(grocery)
 	else:
 		print("your shopping List contains: ")
 		numberItem = 0
-		for item in shoppingList:
+		for grocery in shoppingList:
 			numberItem += 1
-			print(numberItem + " " + item + " ")
-			grocery = input("Add an item: ")
-			shoppingList.append(grocery)
+			print("{}. {}".format(numberItem, grocery)) 
+	grocery = input("Add an item: ")
+	if grocery != "":
+		shoppingList.append(grocery)
+f.seek(0)
+f.truncate()	
+for grocery in shoppingList:
+	f.write(grocery + '\n')
 f.close()
 exit()
